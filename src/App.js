d6aba99fc7f8316aeb4fde9a30c325b09181e468
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-01 13:18:01 +0300

    Import loader component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 /* eslint-disable no-unused-expressions */
 import React from 'react'
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Loader from './Components/Loader'
 import Homepage from './Pages/Homepage'
 import Information from './Pages/Information'
 
@@ -13,17 +10,10 @@ function App() {
   return (
     <>
       <Router>
+        <Loader />
         <Switch>
-          <Route
-            exact
-            path="/"
-            component={Homepage}
-          />
-          <Route
-            exact
-            path="/operator-information"
-            component={Information}
-          />
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/operator-information" component={Information} />
         </Switch>
       </Router>
     </>
